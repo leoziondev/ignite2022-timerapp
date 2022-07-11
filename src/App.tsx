70f87de7ee0b0820fc51d08179f18cc3c1c7ed7f
LeoZion <leozionsuporte@gmail.com>
2022-07-10 23:34:45 -0300

    add default theme and theme typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
