ac4cbdf808a947dacec1f878925f420561c3437b
LeoZion <leozionsuporte@gmail.com>
2022-07-12 16:22:06 -0300

    show timer in tab window

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -68,7 +68,7 @@ export function Home() {
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-    
+
     reset()
   }
   
@@ -80,6 +80,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
